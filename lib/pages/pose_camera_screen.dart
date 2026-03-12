4282331acf5591a5182450d9bd1c9bab73c08f55
Navitha <mallawarachchinavitha22@gmail.com>
2026-03-12 14:24:35 +0530

    Implement basic pose camera preview screen

--- lib/pages/pose_camera_screen.dart
+++ lib/pages/pose_camera_screen.dart
@@ -1,15 +1,63 @@
-import 'dart:io';
-import 'dart:typed_data';
-import 'dart:ui' as ui;
-
 import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
-import 'package:google_mlkit_commons/google_mlkit_commons.dart';
-import 'package:google_mlkit_pose_detection/google_mlkit_pose_detection.dart';
 
 class PoseCameraScreen extends StatefulWidget {
   const PoseCameraScreen({super.key});
 
   @override
   State<PoseCameraScreen> createState() => _PoseCameraScreenState();
+}
+
+class _PoseCameraScreenState extends State<PoseCameraScreen> {
+  CameraController? _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _initCamera();
+  }
+
+  Future<void> _initCamera() async {
+    final cameras = await availableCameras();
+
+    final frontCamera = cameras.firstWhere(
+      (camera) => camera.lensDirection == CameraLensDirection.front,
+      orElse: () => cameras.first,
+    );
+
+    _controller = CameraController(
+      frontCamera,
+      ResolutionPreset.medium,
+      enableAudio: false,
+    );
+
+    await _controller!.initialize();
+
+    if (!mounted) return;
+    setState(() {});
+  }
+
+  @override
+  void dispose() {
+    _controller?.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (_controller == null || !_controller!.value.isInitialized) {
+      return const Scaffold(
+        body: Center(
+          child: CircularProgressIndicator(),
+        ),
+      );
+    }
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Pose Camera'),
+      ),
+      body: CameraPreview(_controller!),
+    );
+  }
 }
