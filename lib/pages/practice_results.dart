9d05c15ad328ce3fc0983a87fb1539e8950f6d32
Ishara Ushan <ishara.20231306@iit.ac.lk>
2026-03-19 18:14:11 +0530

    Completed frontend + dashboard ready

---
+++ lib/pages/practice_results.dart
@@ -0,0 +1,38 @@
+class PracticeResults {
+  final DateTime date;
+  final double postureScore;
+  final double speechScore;
+  final double facialScore;
+  final String? aiFeedback;
+
+  PracticeResults({
+    required this.date,
+    required this.postureScore,
+    required this.speechScore,
+    required this.facialScore,
+    this.aiFeedback,
+  });
+
+  double get overallScore => (postureScore + speechScore + facialScore) / 3;
+
+  Map<String, dynamic> toMap() {
+    return {
+      'date': date.toIso8601String(),
+      'postureScore': postureScore,
+      'speechScore': speechScore,
+      'facialScore': facialScore,
+      'aiFeedback': aiFeedback,
+    };
+  }
+
+  factory PracticeResults.fromMap(Map<String, dynamic> map) {
+    return PracticeResults(
+      date: DateTime.parse(map['date']),
+      postureScore: (map['postureScore'] as num).toDouble(),
+      speechScore: (map['speechScore'] as num).toDouble(),
+      facialScore: (map['facialScore'] as num).toDouble(),
+      aiFeedback: map['aiFeedback'],
+    );
+  }
+}
+
