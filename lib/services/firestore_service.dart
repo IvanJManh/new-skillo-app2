2656cf5e69494927930dd60a82a2a70c7757b1fd
Ishara Ushan <ishara.20231306@iit.ac.lk>
2026-03-20 00:19:19 +0530

    resolved some errors in main branch

--- lib/services/firestore_service.dart
+++ lib/services/firestore_service.dart
@@ -53,4 +53,39 @@ class FirestoreService {
         .doc(skillName)
         .delete();
   }
+
+  Future<List<Map<String, dynamic>>> getSkills() async {
+    final snapshot = await _db.collection('skills').get();
+    return snapshot.docs.map((doc) => {
+      'id': doc.id,
+      ...doc.data(),
+    }).toList();
+  }
+
+  Future<Map<String, dynamic>?> getSkillByTitle(String title) async {
+    final snapshot = await _db
+        .collection('skills')
+        .where('title', isEqualTo: title)
+        .limit(1)
+        .get();
+    if (snapshot.docs.isNotEmpty) {
+      return {
+        'id': snapshot.docs.first.id,
+        ...snapshot.docs.first.data(),
+      };
+    }
+    return null;
+  }
+
+  Future<List<Map<String, dynamic>>> getLessons(String skillId) async {
+    final snapshot = await _db
+        .collection('skills')
+        .doc(skillId)
+        .collection('lessons')
+        .get();
+    return snapshot.docs.map((doc) => {
+      'id': doc.id,
+      ...doc.data(),
+    }).toList();
+  }
 }
